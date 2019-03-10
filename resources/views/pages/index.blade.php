857975300574d3bd40f1b6493f16326020fb72d4
Will Allemang <wallemang@bellarmine.edu>
2019-03-10 00:45:28 -0500

    Initial Commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="jumbotron">
+  <h1>{{ $title }}</h1>
+  <p>This is the laravel application from "laravel from scratch"</p>
+  
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+@endsection
