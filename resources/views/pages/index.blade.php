a241fe17ceb949e13dff9d4722a06f7dc0ece8a6
Will Allemang <wallemang@bellarmine.edu>
2019-03-10 14:59:47 -0400

    Added site hit counter to index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-5">
-       <div class="fb-page" data-href="https://www.facebook.com/chefseanhaggerty/" data-tabs="timeline" data-width="550" data-height="450" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/chefseanhaggerty/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/chefseanhaggerty/">Commonwealth Catering LLC</a></blockquote></div>
-
-    </div>
-    
-    <div class="col-md-7">
+    <div class="col-sm-12 col-md-7">
         <div class="jumbotron">
           <h1>{{ $title }}</h1>
           <p>This is a blog about a weird guy who likes Jeeps, and may not hold any value to anyone</p>
@@ -17,8 +12,12 @@
             <a class="btn btn-primary btn-lg" href="/posts" role="button">See my Posts</a>
           </p>
         </div>
+        <p>Total number of hits: </p> <?php include('storage/counter.php') ?>
+    </div>
+    <div class="col-sm-12 col-md-5">
+        <center><h3>Check out our Facebook page for the latest updates</h3></center>
+        <div class="fb-page" data-href="https://www.facebook.com/chefseanhaggerty/" data-tabs="timeline" data-width="400" data-height="450" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/chefseanhaggerty/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/chefseanhaggerty/">Commonwealth Catering LLC</a></blockquote></div>
+    
     </div>
 </div>
-
-<p>In the mean time, enjoy your stay here..</p>
 @endsection
